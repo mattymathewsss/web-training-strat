b64a26d1980839c6c067f08eb90ae14c1fd91d59
Bryan Matias <bmmatias@stratpoint.com>
2023-03-20 07:03:49 +0000

    feat: added backend functionality

--- components/utility/search/Search.tsx
+++ components/utility/search/Search.tsx
@@ -1,38 +1,45 @@
+import { useRouter } from "next/router";
 import { useState } from "react";
 
-export interface ISearch{}
+export interface ISearch { }
 
 
 const Search: React.FC<ISearch> = () => {
-  const [searchTerm, SetSearchTerm] = useState<string>();
+  const [searchTerm, SetSearchTerm] = useState<string>('');
+
+  const router = useRouter();
+
+  const submitForm = () => {
+    void router.push(`/results?search=${searchTerm}`);
+  }
 
   return (
     <form className="flex flex-col items-center gap-y-5"
-    onSubmit={e => {
-      e.preventDefault();
-      alert(searchTerm)
-    }}>
-    <input
-      type="text"
-      className="rounded-full border-2 w-5/6 sm:w-96 h-12 px-3"
-      value={searchTerm}
-      onChange={(e)=> SetSearchTerm(e.target.value)}
-    />
-    <div className="space-x-3">
-      <button
-        type="submit"
-        className="btn-primary"
-      >
-        Google Search
-      </button>
-      <button
-        type="submit"
-        className="btn-primary"
-      >
-        I&apos;m Feeling Lucky
-      </button>
-    </div>
-  </form>
+      onSubmit={e => {
+        e.preventDefault();
+        submitForm();
+      }}>
+      <input
+        type="text"
+        className="rounded-full border-2 w-5/6 sm:w-96 h-12 px-3"
+        value={searchTerm}
+        onChange={(e) => SetSearchTerm(e.target.value)}
+      />
+      <div className="space-x-3">
+        <button
+          type="submit"
+          className="btn-primary"
+        >
+          Google Search
+        </button>
+        <button
+          type="submit"
+          className="btn-primary"
+        >
+          I&apos;m Feeling Lucky
+        </button>
+      </div>
+    </form>
   );
 };
 
